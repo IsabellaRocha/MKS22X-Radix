412d7655fbabf44c35c46228af7d2f52d233a035
Isabella Rocha <irocha00@stuy.edu>
2019-04-02 00:31:06 -0400

    Wrote more for the for loops

--- Radix.java
+++ Radix.java
@@ -1,8 +1,21 @@
 public class Radix {
   public static void radixsort(int[]data){
     MyLinkedList[] buckets = new MyLinkedList[20];
+    int max = data[0];
     for (int idx = 0; idx < data.length; idx++) {
-      
+      if (max < data[idx]) max = data[idx];
+    }
+    int digits = 0;
+    while (max > 0) {
+      max /= 10;
+      digits++;
+    }
+    for (int idx = 0; idx < digits; idx++) {
+      for (int x = 0; x < data.length; x++) {
+        int digit = getDigit(idx, data[x]);
+        if (data[x] >= 0) buckets[digit + 10].add(data[idx]);
+        if (data[x] < 0) buckets[9 - Math.abs(digit)].add(data[idx]);
+      }
     }
   }
   public static int getDigit(int idx, int num) {
