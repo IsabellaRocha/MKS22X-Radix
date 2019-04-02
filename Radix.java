f31dc3e5453d6c30b502f72d1e92bd3eb9c21aad
Isabella Rocha <irocha00@stuy.edu>
2019-04-02 14:31:29 -0400

    Edited for loops to and cleared buckets

--- Radix.java
+++ Radix.java
@@ -1,27 +1,44 @@
 import java.util.Arrays;
 public class Radix {
   public static void radixsort(int[]data){
-    MyLinkedList[] buckets = new MyLinkedList[20];
+    @SuppressWarnings("unchecked")
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
     int max = data[0];
     for (int idx = 0; idx < buckets.length; idx++) {
-      buckets[idx] = new MyLinkedList();
+      buckets[idx] = new MyLinkedList<Integer>();
     }
     for (int idx = 0; idx < data.length; idx++) {
       if (max < data[idx]) max = data[idx];
     }
+    System.out.println(max);
     int digits = 0;
     while (max > 0) {
       max /= 10;
       digits++;
     }
-    for (int idx = 1; idx < digits; idx++) {
+    System.out.println(digits);
+    MyLinkedList<Integer> temp = new MyLinkedList();
+    for (int idx = digits; idx > 0; idx--) {
       for (int x = 0; x < data.length; x++) {
         int digit = getDigit(idx, data[x]);
-        if (data[x] >= 0) buckets[digit + 10].add(data[idx]);
-        if (data[x] < 0) buckets[9 - Math.abs(digit)].add(data[idx]);
+        if (data[x] >= 0) {
+          buckets[digit + 10].add(data[x]);
+          temp.extend(buckets[digit + 10]);
+        }
+        if (data[x] < 0) {
+          buckets[9 - Math.abs(digit)].add(data[x]);
+          buckets[9 - Math.abs(digit)].extend(temp);
+        }
+      }
+      for (int i = 0; i < data.length; i++) {
+        data[i] = (int) temp.removeFront();
+      }
+      temp.clear();
+      for (int i = 0; i < buckets.length; i++) {
+        buckets[i].clear();
       }
     }
-    MyLinkedList output = new MyLinkedList();
+    MyLinkedList<Integer> output = new MyLinkedList();
     for (int idx = 0; idx < buckets.length; idx++) {
       output.extend(buckets[idx]);
     }
