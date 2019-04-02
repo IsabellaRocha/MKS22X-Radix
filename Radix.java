6b52fe642d6134d3e5381ac0594a878d67fffdb7
Isabella Rocha <irocha00@stuy.edu>
2019-04-02 17:30:43 -0400

    Made progress, no errors thrown

--- Radix.java
+++ Radix.java
@@ -15,37 +15,48 @@ public class Radix {
       max /= 10;
       digits++;
     }
-    MyLinkedList<Integer> temp = new MyLinkedList();
+    MyLinkedList<Integer> store = new MyLinkedList();
     for (int idx = digits; idx > 0; idx--) {
       for (int x = 0; x < data.length; x++) {
-        int digit = getDigit(idx, data[x]);
-        if (data[x] >= 0) buckets[digit + 10].add(data[x]);
-        if (data[x] < 0) buckets[9 - Math.abs(digit)].add(data[x]);
+        int digit = (data[x] / (int) Math.pow(10, idx)) % 10;
+        if (data[x] > 0) buckets[digit + 10].add(data[x]);
+        else buckets[9 - Math.abs(digit)].add(data[x]);
       }
       for (int i = 0; i < buckets.length; i++) {
-        temp.extend(buckets[i]);
+        store.extend(buckets[i]);
       }
       for (int i = 0; i < data.length; i++) {
-        data[i] = (int)temp.removeFront();
+        data[i] = (int)store.removeFront();
       }
-      temp.clear();
-      for (int i = 0; i < buckets.length; i++) {
-        buckets[i].clear();
-      }
-    }
-    MyLinkedList<Integer> output = new MyLinkedList();
-    for (int idx = 0; idx < buckets.length; idx++) {
-      output.extend(buckets[idx]);
-    }
-    for (int idx = 0; idx < data.length; idx++) {
-      data[idx] = (int)output.removeFront();
     }
   }
   public static int getDigit(int idx, int num) {
-    return (int)(num / Math.pow(10, idx)) % 10;
+    int temp = num;
+    int dig = 0;
+    while (temp != 0) {
+      dig++;
+      temp /= 10;
+    }
+//    System.out.println(num);
+//    System.out.println(dig);
+    if (idx > dig) return 0;
+    if (idx == dig) {
+      while (num > 10) {
+  //      System.out.println(num);
+        num /= 10;
+      }
+      return num % 10;
+    }
+    while (num > Math.pow(10, idx)) {
+//      System.out.println(num);
+      num /= 10;
+    }
+    return num % 10;
   }
   public static void main(String[] args) {
-    System.out.println(getDigit(1, 15));
+    System.out.println(getDigit(3, 200));
+    System.out.println(getDigit(5, 34245));
+    System.out.println(getDigit(3, 5));
     System.out.println(getDigit(4, 12345));
     System.out.println(getDigit(6, 123456789));
     int[] data = {30, 5, 200, 1, -5, 68};
